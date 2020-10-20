76edd8e27a1af900e26759e8b8fd833adb77bbf9
ganhaobo <ganhaobo@meicai.cn>
2020-10-20 13:36:08 +0800

    fix: dev配置修复完毕

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,29 +1,36 @@
-const { mergeWithCustomize, customizeArray } = require('webpack-merge'),
+const {
+        mergeWithCustomize, customizeArray, customizeObject
+    } = require('webpack-merge'),
     Common = require('./webpack.common'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
-    { resolve } = require('path')
+    { resolve } = require('path'),
+    finalConfig = mergeWithCustomize({
+        customizeArray: customizeArray({ '*': 'append' }),
+        customizeObject: customizeObject({ output: 'replace' })
+    })(Common, {
+        mode: 'development',
+        devtool: 'inline-source-map',
 
-module.exports = mergeWithCustomize({ customizeArray: customizeArray({ '*': 'append' }) })(Common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+        devServer: {
+            contentBase: resolve(__dirname, '../dist'),
+            hot: true,
+            stats: 'errors-only'
+        },
 
-    devServer: {
-        http2: true,
-        contentBase: resolve(__dirname, '../dist'),
-        hot: true,
-        stats: 'errors-only'
-    },
+        entry: { app: resolve(__dirname, '../src/index.js') },
 
-    entry: { app: resolve(__dirname, '../src/index.js') },
+        output: {
+            filename: '[name].bundle.js',
+            path: resolve(__dirname, '../dist')
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: '组件开发服务器',
+                template: resolve(__dirname, '../dev/index.html')
+            })
+        ]
+    })
 
-    output: {
-        filename: '[name].bundle.js',
-        path: resolve(__dirname, '../dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '组件开发服务器',
-            template: resolve(__dirname, '../dev/index.html')
-        })
-    ]
-})
+delete finalConfig.externals
+
+module.exports = finalConfig
